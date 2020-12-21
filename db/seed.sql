7b6414e762bac5d53c62165a75a66930d6769b02
Shivey Singh <shivey_khangura@hotmail.com>
2020-12-21 22:34:40 +0000

    Update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
 INSERT INTO burgers (burger_name) VALUES ('Daddy Burger');
 INSERT INTO burgers (burger_name) VALUES ('Thanos Burger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Ring Of Fire  Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ring Of Fire Burger', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Put Some Hairs On Your Chest Burger', true);
